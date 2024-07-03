e795f6f11e3a30e57d235083354af726377d6220
simba88888 <1006684039@qq.com>
2024-07-03 22:24:10 +0800

    free for github

---
+++ addons/yy_shequn2/template/mobile/images/js/base.js
@@ -0,0 +1,13 @@
+﻿(function(){
+
+})();
+
+$(function(){
+	
+	$(document.body).css("min-height",$(window).height());
+	$(window).resize(function(){
+		document.getElementsByTagName('html')[0].style.fontSize = Math.min(window.innerWidth*12/320,200)+"px";		
+		$(document.body).css("min-height",$(window).height());
+	});
+
+});	
